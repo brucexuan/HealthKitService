596ffab77b202e026ca9003de0860f92b29c68e6
Bruce Xuan <xuan_chao@hotmail.com>
2015-11-01 19:22:54 +0800

    New added class for admin user and privilege

---
+++ HealthKitService/Models/AdminUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HealthKitService.Models
+{
+    public class AdminUser
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string RealName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ HealthKitService/Models/Privilege.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HealthKitService.Models
+{
+    public class Privilege
+    {        
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Category { get; set; }
+    }
+}
